fd38af13009c562e6a039dd0240292890ce73a5b
mrvacbob <mrvacbob@users.noreply.github.com>
2014-04-01 23:12:13 -0700

    Fix handling of >< or +- (Fixes #1)
    Also use putchar/getchar_unlocked in to-c.

--- toc-interpreter.c
+++ toc-interpreter.c
@@ -14,7 +14,6 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #define NO_JNZ
-#define NO_PRINT_IR
 #include "skeleton.h"
 
 static void emit_c()
@@ -39,8 +38,8 @@ static void emit_c()
 			def(Inc1, "(*tape)++")
 			defv(Dec, "(*tape) -= %u")
 			def(Dec1, "(*tape)--")
-			def(Putchar, "putchar(*tape)")
-			def(Getchar, "*tape = getchar()")
+			def(Putchar, "putchar_unlocked(*tape)")
+			def(Getchar, "*tape = getchar_unlocked()")
 			defb(Jz, "while (*tape) {")
 			defb(Jmp, "}")
 			def(Set0, "(*tape) = 0")
